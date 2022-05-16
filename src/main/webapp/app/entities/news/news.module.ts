57ccf44828278b05e25c4a5c6ea34304ee67fdc2
Shreekantha D K <shreekanth.karikalli@gmail.com>
2022-05-16 18:36:53 +0530

    news

--- src/main/webapp/app/entities/news/news.module.ts
+++ src/main/webapp/app/entities/news/news.module.ts
@@ -1,15 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
-
 import { IgrscmsSharedModule } from 'app/shared/shared.module';
-import { NewsComponent } from './news.component';
+import { CKEditorModule } from 'ckeditor4-angular';
+import { NewsDeleteDialogComponent } from './news-delete-dialog.component';
 import { NewsDetailComponent } from './news-detail.component';
 import { NewsUpdateComponent } from './news-update.component';
-import { NewsDeleteDialogComponent } from './news-delete-dialog.component';
+import { NewsComponent } from './news.component';
 import { newsRoute } from './news.route';
 
 @NgModule({
-  imports: [IgrscmsSharedModule, RouterModule.forChild(newsRoute)],
+  imports: [IgrscmsSharedModule, CKEditorModule, RouterModule.forChild(newsRoute)],
   declarations: [NewsComponent, NewsDetailComponent, NewsUpdateComponent, NewsDeleteDialogComponent],
   entryComponents: [NewsDeleteDialogComponent],
 })
